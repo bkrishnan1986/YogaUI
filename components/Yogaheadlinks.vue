ef30a97ab67d421ebddd36760a12358f28028a07
Bala <bkishore@brightfind.com>
2022-11-19 10:54:33 +0530

    Update css path

--- components/Yogaheadlinks.vue
+++ components/Yogaheadlinks.vue
@@ -3,19 +3,19 @@
     <title>
         Yoga Alliance
     </title>
-    <link href="_nuxt/css/default.css" type="text/css" rel="stylesheet"/>
+    <link href="css/default.css" type="text/css" rel="stylesheet"/>
     <link href="https://www.yogaalliance.org/Portals/0/skins/yogav2/css/pages/apply-for-rys-credential.css?cdv=425" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/adminyoga.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/SearchSkinObjectPreview.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/skin.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/container.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/portal.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/StandardMenu.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/optin-form.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/header.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/screen.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/new-homepage.css" type="text/css" rel="stylesheet"/>
-    <link href="_nuxt/css/flat-buttons.css" type="text/css" rel="stylesheet"/>
+    <link href="css/adminyoga.css" type="text/css" rel="stylesheet"/>
+    <link href="css/SearchSkinObjectPreview.css" type="text/css" rel="stylesheet"/>
+    <link href="css/skin.css" type="text/css" rel="stylesheet"/>
+    <link href="css/container.css" type="text/css" rel="stylesheet"/>
+    <link href="css/portal.css" type="text/css" rel="stylesheet"/>
+    <link href="css/StandardMenu.css" type="text/css" rel="stylesheet"/>
+    <link href="css/optin-form.css" type="text/css" rel="stylesheet"/>
+    <link href="css/header.css" type="text/css" rel="stylesheet"/>
+    <link href="css/screen.css" type="text/css" rel="stylesheet"/>
+    <link href="css/new-homepage.css" type="text/css" rel="stylesheet"/>
+    <link href="css/flat-buttons.css" type="text/css" rel="stylesheet"/>
     <link rel="apple-touch-icon" sizes="57x57" href="https://www.yogaalliance.org/apple-touch-icon-57x57.png"/>
     <link rel="apple-touch-icon" sizes="114x114" href="https://www.yogaalliance.org/apple-touch-icon-114x114.png"/>
     <link rel="apple-touch-icon" sizes="72x72" href="https://www.yogaalliance.org/apple-touch-icon-72x72.png"/>
@@ -30,7 +30,7 @@
     <link rel="icon" type="image/png" href="https://www.yogaalliance.org/favicon-32x32.png" sizes="32x32"/>
     <link rel="icon" type="image/png" href="https://www.yogaalliance.org/favicon-16x16.png" sizes="16x16"/>
     <link rel="stylesheet" type="text/css" href="https://www.yogaalliance.org/Portals/0/Skins/YogaV2/skin-css-partials/layout.css"/>
-    <link rel="stylesheet" type="text/css" href="_nuxt/css/app.common.css"/>
-    <link rel="stylesheet" type="text/css" href="_nuxt/css/app.common.css"/>
+    <link rel="stylesheet" type="text/css" href="css/app.common.css"/>
+    <link rel="stylesheet" type="text/css" href="css/app.common.css"/>
     </head>
 </template>
